804cea814ce86866a92385c17ec4d87747731654
Lindell Xu <lxu@vetronicsltd.com.cn>
2013-09-02 11:26:42 +0800

    reformat
    Signed-off-by: Lindell Xu <lxu@vetronicsltd.com.cn>

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -4,16 +4,16 @@
 int
 main(int argc, char **argv)
 {
-	int					listenfd, connfd;
+	int			listenfd, connfd;
 	struct sockaddr_in	servaddr;
-	char				buff[MAXLINE];
-	time_t				ticks;
+	char			buff[MAXLINE];
+	time_t			ticks;
 
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); /* INADDR_ANY means for any address */
 	servaddr.sin_port        = htons(13);	/* daytime server */
 
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
